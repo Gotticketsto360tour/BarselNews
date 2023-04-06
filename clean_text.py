e7e9cfc93f671f58c062364db83ecedf0f0f8a25
Gotticketsto360tour <werling1407@gmail.com>
2023-04-06 12:15:49 +0200

    edit names + make constants file

--- clean_text.py
+++ clean_text.py
@@ -2,77 +2,29 @@ import pandas as pd
 import string
 import spacy
 from tqdm import tqdm
+from constants import LIST_OF_RIGHT, LIST_OF_LEFT, LIST_OF_STOPPERS, MAPPING
 
 # load the data
-d = pd.read_csv("data/Article_sentiment.csv", sep=";")
-d_pol = pd.read_csv("data/data_political1.csv")
+d = pd.read_csv("data/barsel_sentiment.csv", sep=";")
 
 # calculate the number of words per sentence
 d["length"] = d["sentences"].apply(lambda x: len(x.split()))
 # exclude sentences of less than three words
 d = d[d["length"] > 2].reset_index(drop=True)
-# merge with political data to include political orientation for articles
-political = d_pol[["ID", "Political_Orientation"]]
-d = d.merge(political, on="ID")
 
-# list of endings of articles that need to excluded from the texts
-list_of_stoppers = [
-    "LINEA SØGAARD-LIDELL EU-parlamentariker, Venstre.",
-    " I LLU ST RATION: ",
-    " Kilde: Beskæftigelsesministeriet. ,",
-    " dorte.boddum@finans.dk",
-    " mie.l.raatz@jp.dk ",
-    " segj@information.dk",
-    ". (arkivfoto)",
-    " FOTO: ",
-    " frederikke.traeholt@jp.dk",
-    ". Ritzau.",
-    "Camilla Gregersen Dansk Magisterforening.",
-    " makr@information.dk",
-    "MICHELLA MEIER-MORSI. , Michella Meier-Morsi og Mark Morsi med deres fem børn. Privatfoto",
-    " (ARKIV)",
-    " loka@berlingske.dk , ",
-    " jonas.proschold@pol.dk",
-    "SIDE 8-9. FOTO: ",
-    " elisabet.svane@pol.dk ,",
-    " ILLUSTRATION: ",
-    "Kilder: EU-",
-    "Kilder: Folketingets",
-    "Kilde: \\ ",
-    "Kilder: Danmarks Statistik, Politiken, Ritzau. ",
-    " sonne@information.dk ",
-    " , laura.nissen@jp.dk",
-    " PR-foto",
-    "Ark kivfoto: ",
-    "anders.m.bruun@finans.dk Illustration: ",
-    "winther@k.dk Fakta:",
-    "FOTO: PER",
-    "Fakta: FAKTA ",
-    "Fejl og Fakta 28.10.2021:",
-    "Fakta: Hvad mener du? Send ",
-    " ELSE JOHANNESSEN,",
-    " Fakta: BLÅ BOG ",
-    "Se alle 140 underskrivere på information.dk/ deltag.",
-    " (Foto: ",
-    " (© COLORBOX) ",
-    "hejs@berlingske.dk tbre@berlingske.dk",
-    " KILDER: ",
-    " information. dk/deltag ",
-    " Arkivfoto: ",
-    "Se klippet fra samtalen mellem Alex Vanopslagh og Søren Pind her.",
-    "LÆS OGSÅ KRONIKKEN SIDE ",
-    "PRIVAT",
-    " Berlingske Grafik: ",
-    "/ ritzau/",
-    " Læs mere her. , FOLD UD ",
-    "Alt mediemateriale fra Infomedia er ophavsretligt beskyttet.",
-    "/Ritzau/",
-    " Fold sammen Læs mere ",
-    "/ritzau/",
-    " Foto: ",
-]
+
+def get_pol_orientation(string):
+    if string in LIST_OF_LEFT:
+        return "Left-winged"
+    if string in LIST_OF_RIGHT:
+        return "Right-winged"
+    else:
+        return None
+
+d["Political_Oirentation"] = d["Newspaper"].apply(get_pol_orientation)
+
 # loop through and take text before the list of stoppers
-for stopper in list_of_stoppers:
+for stopper in LIST_OF_STOPPERS:
     d["article"] = d["article"].apply(lambda x: x.split(stopper)[0])
 d = d.reset_index()
 
@@ -81,9 +33,6 @@ texts = d["article"].values
 # loading in the danish news model that is used to clean data
 nlp = spacy.load("da_core_news_lg")
 
-
-# drop duplicates to have a row per article
-# d_tokens = d.drop_duplicates("merge_index").reset_index(drop=True)
 d_tokens = d.copy()
 
 # POS Tags to remove from tokens
@@ -115,82 +64,12 @@ d_long["word"] = d_long["tokens"].apply(
 
 counts = d_long["word"].value_counts().reset_index()
 
-mapping = {
-    "socialdemokratisk": "socialdemokrati",
-    "sammenligning": "sammenligne",
-    "regle": "regler",
-    "parret": "par",
-    "orlovsug": "orlov",
-    "økonom": "økonomi",
-    "økonomisk": "økonomi",
-    "økonomii": "økonomi",
-    "økonomiisk": "økonomi",
-    "mødre": "mor",
-    "moder": "mor",
-    "mødr": "mor",
-    "mødrene": "mor",
-    "mødree": "mor",
-    "mødree": "mor",
-    "mødreene": "mor",
-    "modell": "model",
-    "mænds": "mand",
-    "mændene": "mand",
-    "kvinder": "kvinde",
-    "kvinders": "kvinde",
-    "kvinderne": "kvinde",
-    "kvind": "kvinde",
-    "kvinderr": "kvinde",
-    "kvinderrelig": "kvinde",
-    "kvinderrer": "kvinde",
-    "kvinderererererre": "kvinde",
-    "kvindererererer": "kvinde",
-    "konsekvense": "konsekvens",
-    "konkre": "konkret",
-    "idé": "ide",
-    "forvej": "forvejen",
-    "forældr": "forældre",
-    "famili": "familie",
-    "familiee": "familie",
-    "fagbevægelsen": "fagbevægelse",
-    "fagbevægels": "fagbevægelse",
-    "fagbevægelsenens": "fagbevægelse",
-    "fars": "far",
-    "fædr": "far",
-    "fædrene": "far",
-    "fædrenes": "far",
-    "fædres": "far",
-    "fader": "far",
-    "farene": "far",
-    "farenes": "far",
-    "fares": "far",
-    "barselsug": "barselsuge",
-    "barsle": "barsel",
-    "barslenn": "barsel",
-    "barslen": "barsel",
-    "barselslovgivning": "barselslov",
-    "barselsorloven": "barselsorlov",
-    "barselsregler": "barselsregel",
-    "barselsregle": "barselsregel",
-    "barselsreglerrr": "barselsregel",
-    "barselsreglerr": "barselsregel",
-    "barselsugeee": "barselsuge",
-    "barselsugee": "barselsuge",
-    "børn": "barn",
-    "børnenes": "barn",
-    "børns": "barn",
-    "forældree": "forældre",
-    "konkrett": "konkret",
-    "ligestillingsminist": "ligestillingsminister",
-    "ligestillingsordføre": "ligestillingsordfører",
-    "fagbevægelsenenenenen": "fagbevægelse",
-}
-
 with open("stopwords/stopwords.txt") as f:
     stopwords_list = f.read()
 
 stopwords = stopwords_list.split("\n")
 
-mapping_list = list(mapping.keys())
+mapping_list = list(MAPPING.keys())
 
 
 def map_words(word, mapping, mapping_list):
@@ -200,7 +79,7 @@ def map_words(word, mapping, mapping_list):
         return word
 
 
-d_long["word"] = d_long["word"].apply(lambda x: map_words(x, mapping, mapping_list))
+d_long["word"] = d_long["word"].apply(lambda x: map_words(x, MAPPING, mapping_list))
 
 data = d_long[~d_long["word"].isin(stopwords)]
 
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -3,46 +3,30 @@
 import pandas as pd
 from danlp.models import load_bert_tone_model
 from tqdm import tqdm
+tqdm.pandas()
 
-data = pd.read_csv("data/average_article_sentences_.csv", sep=",")
+barsel_data = pd.read_csv("data/barsel_sentences.csv", sep=";")
+control_data = pd.read_csv("data/control_sentences.csv", sep=",")
 
-# load the tone model and call it classifier
+# load the tone model
 classifier = load_bert_tone_model()
-
-# get the comments
-texts = data["sentences"].values
 
 # SENTIMENT ANALYSIS
 # _______________
 
-# make stupid lists to store the data
-positive_prob = []
-neutral_prob = []
-negative_prob = []
-objective_prob = []
-subjective_prob = []
-
-for comment in tqdm(texts):
-    # get the prediction probabilities for each comment
+def sentiment_analysis(comment: str):
     probs = classifier.predict_proba(comment)
-    # split the probabilities into whether it's sentiment of objective
     sentiment, angle = probs[0], probs[1]
+    return sentiment[0], sentiment[1], sentiment[2], angle[0], angle[1]
 
-    # store the data
-    positive_prob.append(sentiment[0])
-    neutral_prob.append(sentiment[1])
-    negative_prob.append(sentiment[2])
-    objective_prob.append(angle[0])
-    subjective_prob.append(angle[1])
+for data in [barsel_data, control_data]:
+    (
+        data["positive_prob"],
+        data["neutral_prob"],
+        data["negative_prob"],
+        data["objective_prob"],
+        data["subjective_prob"],
+    ) = data["sentences"].apply(sentiment_analysis)
 
-# add the data to the data frame
-(
-    data["positive_prob"],
-    data["neutral_prob"],
-    data["negative_prob"],
-    data["objective_prob"],
-    data["subjective_prob"],
-) = (positive_prob, neutral_prob, negative_prob, objective_prob, subjective_prob)
-
-# store the csv
-data.to_csv("data/average_article_sentences_probs.csv", sep=";", index=False)
+barsel_data.to_csv("data/barsel_sentiment.csv", sep=";", index=False)
+control_data.to_csv("data/control_sentiment.csv", sep=";", index=False)
